ea261a29627cf9d594f8e7022feeb8b53281d6be
manizheh1 <Manizheh.Zand@gmail.com>
2023-09-07 14:50:23 -0700

    update bat

---
+++ install.bat
@@ -0,0 +1,11 @@
+@echo off
+
+title Install Script!
+echo Installing...
+conda create -n pip_summer_school -y -c conda-forge jupyterlab=3 "ipykernel>=6" xeus-python python=3.8
+conda install -y scipy
+conda install -y -c conda-forge pandas
+conda install -y -c conda-forge scikit-learn
+conda install -y -c conda-forge statsmodels
+conda install -y -c conda-forge matplotlib
+conda install -y -c conda-forge seaborn
