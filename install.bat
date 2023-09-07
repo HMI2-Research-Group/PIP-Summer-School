fdb6e8cca1d11995feba5ee35ddbfdaa3a52ec0b
manizheh1 <Manizheh.Zand@gmail.com>
2023-09-07 14:52:08 -0700

    fix bug

--- install.bat
+++ install.bat
@@ -3,6 +3,7 @@
 title Install Script!
 echo Installing...
 conda create -n pip_summer_school -y -c conda-forge jupyterlab=3 "ipykernel>=6" xeus-python python=3.8
+conda activate pip_summer_school
 conda install -y scipy
 conda install -y -c conda-forge pandas
 conda install -y -c conda-forge scikit-learn
